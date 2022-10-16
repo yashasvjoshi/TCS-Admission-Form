1286cb1ad6e393193914be366cf2b729588334d2
yashasv7 <115701902+yashasv7@users.noreply.github.com>
2022-10-16 13:10:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let value1 = document.getElementById("1value");
+
+let iarg = document.getElementById("1arg")
+let btn = document.getElementById("s-btn")
+
+
+btn.addEventListener('click', () => {
+    location.reload()
+})
